a260a88599fa615b1507aeb61de0e7308a3378e9
Adrian Fullmer <ipkcle@gmail.com>
2018-12-01 17:12:49 -0500

    made solution simpler

--- chronal-calibration/solution.lisp
+++ chronal-calibration/solution.lisp
@@ -1,19 +1,17 @@
-(defun part-one ()
+(defun get-frequency-changes ()
   (let ((input (open "input" :if-does-not-exist nil))
-        (total 0))
-    (loop for line = (read-line input nil)
-          while line do (setf total (+ total (parse-integer line))))
-    (close input)
-    total))
-
-(defun part-two ()
-  (let ((input (open "input" :if-does-not-exist nil))
-        (frequency-changes (make-array 5 :fill-pointer 0 :adjustable t))
-        (frequency 0)
-        (visited-frequencies (make-hash-table)))
+        (frequency-changes (make-array 5 :fill-pointer 0 :adjustable t)))
     (loop for line = (read-line input nil)
           while line do (vector-push-extend (parse-integer line) frequency-changes))
-    (loop (loop for change across frequency-changes
+    frequency-changes))
+
+(defun part-one ()
+  (reduce #'+ (get-frequency-changes)))
+
+(defun part-two ()
+  (let ((visited-frequencies (make-hash-table))
+        (frequency 0))
+    (loop (loop for change across (get-frequency-changes)
                 if (gethash frequency visited-frequencies) do
                   (return-from part-two frequency)
                 else do (setf (gethash frequency visited-frequencies) t)
